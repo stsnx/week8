68ffec64b59d23d2dacca63a6cfc04f3a587a752
stsnx <69145525+stsnx@users.noreply.github.com>
2020-10-14 01:01:22 +0700

    Add files via upload

---
+++ Week8_1/Week8_1/Source.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+struct info {
+	int score;
+	char name[60];
+}student[100];
+bool sortin(info x, info y)
+{
+	return x.score > y.score;
+}
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> student[i].name;
+		int sum = 0;
+		for (int j = 0; j < 3; j++)
+		{
+			int temp;
+			cin >> temp;
+			sum += temp;
+		}
+		student[i].score = sum;
+		if (student[i].score > 100 || student[i].score < 0)
+		{
+			student[i].score = -1;
+		}
+	}
+	sort(student, student + n, sortin);
+	for (int i = 0; i < n; i++)
+	{
+		if (student[i].score > 100 || student[i].score < 0)
+		cout << student[i].name <<" error"<< endl;
+		else 
+		cout << student[i].name << ' ' << student[i].score << endl;
+	}
+	return 0;
+}
+
